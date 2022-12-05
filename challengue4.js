e53bb9e4381ace659cca7325f198fe53aabec289
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-12-05 19:10:31 -0300

    Challengue 4

--- challengue4.js
+++ challengue4.js
@@ -30,36 +30,80 @@
 
 const boxes = [
     { l: 1, w: 1, h: 1 },
+    { l: 3, w: 1, h: 3 },
     { l: 2, w: 2, h: 2 },
-    { l: 3, w: 1, h: 3 }
 ]
 const boxes2 = [
     { l: 1, w: 1, h: 1 },
     { l: 2, w: 2, h: 2 }
 ] // true
+
 // sort
 function fitsInOneBox(boxes) {
+    let theReturn
 
-    // console.log(boxes);
+    let boxesSort = boxes.sort((a, b) => { 
+       return a.l - b.l && a.h - b.h && a.w - b.w
+    });
+  console.log(boxesSort);
+    for (let i = 0; i < boxesSort.length - 1; i++) {
+        const element = boxesSort[i]
+        const element2 = boxesSort[i + 1]
 
-    // boxes.forEach((box) => {
-    //     console.log("l:", box.l);
-    //     console.log("w:", box.w);
-    //     console.log("h:", box.h);
-
-    // });
-
-
-
-    for (const key in boxes) {
-        console.log(boxes[key]);
+        if ((element.l < element2.l) && (element.w < element2.w) && (element.h < element2.h)) {
+            console.log(element.h, element2.h);
+            theReturn = true
+            
+        } else {
+            theReturn = false
+            break;
+        }
     }
 
+    console.log(theReturn);
+    return theReturn
 
 }
+    
+// fitsInOneBox(boxes2) // true
+// console.log(fitsInOneBox(boxes)) // false
+fitsInOneBox([
+    { l: 1, w: 1, h: 10 },
+    { l: 3, w: 3, h: 12 },
+    { l: 2, w: 2, h: 1 },
+]
+) // false
 
-fitsInOneBox(boxes2) // true
 
 
+// function fitsInOneBox(boxes) {
+//     let theReturn
+
+//     let boxesSort = boxes.sort((a, b) => { 
+//         a.l - b.l
+//     });
+//     console.log(boxes);
+//     console.log(boxesSort);
+  
+//     for (let i = 0; i < boxesSort.length - 1; i++) {
+//         const element = boxesSort[i]
+//         const element2 = boxesSort[i + 1]
+
+//         if ((element.l < element2.l) && (element.w < element2.w) && (element.h < element2.h)) {
+//             console.log("true");
+//             console.log(element);
+//             console.log(element2);
+
+//             theReturn = true
+//         } else {
+//             console.log("false");
+//             theReturn = false
+//         }
+//     }
 
 
+//     console.log(theReturn);
+//     return theReturn
+
+
+// }
